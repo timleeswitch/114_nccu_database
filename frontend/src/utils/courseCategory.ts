348347250e74375aef9ea56723d8b249149c2c63
Allison Chang <ic995115@gmail.com>
2026-06-03 01:50:44 +0800

    feat: add utils (from #10 credits_record_v2)

---
+++ frontend/src/utils/courseCategory.ts
@@ -0,0 +1,18 @@
+import type { CourseCategory } from '../types/course';
+
+export function getCourseCategoryLabel(category: CourseCategory | string): string {
+  switch (category) {
+    case 'required':
+      return '必修';
+    case 'core_elective':
+      return '群修';
+    case 'general':
+      return '通識';
+    case 'free_elective':
+      return '選修';
+    case 'other':
+      return '其他';
+    default:
+      return '其他';
+  }
+}
