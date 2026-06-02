8dd10b031152822b33c694cdc213a2f978dddc23
Allison Chang <ic995115@gmail.com>
2026-06-03 01:57:51 +0800

    feat: add routes for course records and credit check pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import GraduationStatusPage from './pages/GraduationStatusPage';
+import CourseRecordsPage from './pages/CourseRecordsPage';
+import CreditCheckPage from './pages/CreditCheckPage';
 import './index.css';
 
 export default function App() {
@@ -9,6 +11,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/status" element={<GraduationStatusPage />} />
+        <Route path="/records" element={<CourseRecordsPage studentId={1} />} />
+        <Route path="/credits" element={<CreditCheckPage />} />
       </Routes>
     </BrowserRouter>
   );
